5620fe335eeae23617f6c536514f571201f33d4c
pechenushk4 <uliaershova08280@gmail.com>
2026-04-23 21:56:32 +0300

    fix test import

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,6 +10,8 @@ from sqlalchemy.orm import Session
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import Base, Book
 
 # ─────────────────────────────────────────────
